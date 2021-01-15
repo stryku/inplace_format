bf555e6dbbfae87844875654121779b3cb4ab6d0
stryku <stryku2393@gmail.com>
2021-01-15 21:34:55 +0100

    Added rest of ints

--- infmt.hpp
+++ infmt.hpp
@@ -73,8 +73,22 @@ constexpr auto calc_size(std::string_view s)
 
     const auto end_pos = current.find('}', begin_pos);
     const auto subs = current.substr(begin_pos, end_pos + 1u);
-    if (subs == "{int32}") {
+    if (subs == "{uint8}") {
+      size += std::numeric_limits<std::uint8_t>::digits10;
+    } else if (subs == "{int8}") {
+      size += std::numeric_limits<std::int8_t>::digits10 + 1u; // 1u for minus
+    } else if (subs == "{uint16}") {
+      size += std::numeric_limits<std::uint16_t>::digits10;
+    } else if (subs == "{int16}") {
+      size += std::numeric_limits<std::int16_t>::digits10 + 1u; // 1u for minus
+    } else if (subs == "{uint32}") {
+      size += std::numeric_limits<std::uint32_t>::digits10;
+    } else if (subs == "{int32}") {
       size += std::numeric_limits<std::int32_t>::digits10 + 1u; // 1u for minus
+    } else if (subs == "{uint64}") {
+      size += std::numeric_limits<std::uint64_t>::digits10;
+    } else if (subs == "{int64}") {
+      size += std::numeric_limits<std::int64_t>::digits10 + 1u; // 1u for minus
     }
     current = current.substr(end_pos + 1u);
   }
--- test.cpp
+++ test.cpp
@@ -4,7 +4,18 @@ int main()
 {
   static_assert(infmt::details::calc_size("") == 0);
   static_assert(infmt::details::calc_size(" ") == 1);
+
+  static_assert(infmt::details::calc_size("{uint8}") == 2);
+  static_assert(infmt::details::calc_size("{int8}") == 3);
+
+  static_assert(infmt::details::calc_size("{uint16}") == 4);
+  static_assert(infmt::details::calc_size("{int16}") == 5);
+
+  static_assert(infmt::details::calc_size("{uint32}") == 9);
   static_assert(infmt::details::calc_size("{int32}") == 10);
+
+  static_assert(infmt::details::calc_size("{uint64}") == 19);
+  static_assert(infmt::details::calc_size("{int64}") == 19);
 
   constexpr auto formatter = infmt::make_formatter("{int32}");
 }
