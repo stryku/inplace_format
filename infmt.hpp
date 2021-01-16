727b258155a8982c5828db30b1254c726bb8aee8
stryku <stryku2393@gmail.com>
2021-01-16 10:41:20 +0100

    Implemented format_param_from for ints

--- infmt.hpp
+++ infmt.hpp
@@ -4,6 +4,26 @@
 #include <string_view>
 #include <tuple>
 
+#define INFMT_STRING(s)                                                       \
+  [] {                                                                        \
+    struct str                                                                \
+    {                                                                         \
+      static constexpr auto to_string_view()                                  \
+      {                                                                       \
+        return std::string_view{ s };                                         \
+      }                                                                       \
+                                                                              \
+      static constexpr auto substr(std::string_view::size_type pos,           \
+                                   std::string_view::size_type count)         \
+      {                                                                       \
+        return to_string_view().substr(pos, count);                           \
+      }                                                                       \
+                                                                              \
+      static constexpr decltype(auto) to_raw_str() { return s; }              \
+    };                                                                        \
+    return str{};                                                             \
+  }()
+
 namespace infmt {
 
 namespace details {
@@ -97,6 +117,47 @@ constexpr unsigned length_of(std::string_view s)
   return s.size();
 }
 
+template <unsigned CurrentPos, typename S>
+constexpr auto format_param_from(S)
+{
+  constexpr auto length = length_of(S::to_string_view());
+  if constexpr (S::substr(CurrentPos, 7u) == "{uint8}") {
+    return format_param<std::uint8_t, CurrentPos, length>{};
+  } else if constexpr (S::substr(CurrentPos, 6u) == "{int8}") {
+    return format_param<std::int8_t, CurrentPos, length>{};
+  } else if constexpr (S::substr(CurrentPos, 8u) == "{uint16}") {
+    return format_param<std::uint16_t, CurrentPos, length>{};
+  } else if constexpr (S::substr(CurrentPos, 7u) == "{int16}") {
+    return format_param<std::int16_t, CurrentPos, length>{};
+  } else if constexpr (S::substr(CurrentPos, 8u) == "{uint32}") {
+    return format_param<std::uint32_t, CurrentPos, length>{};
+  } else if constexpr (S::substr(CurrentPos, 7u) == "{int32}") {
+    return format_param<std::int32_t, CurrentPos, length>{};
+  } else if constexpr (S::substr(CurrentPos, 8u) == "{uint64}") {
+    return format_param<std::uint64_t, CurrentPos, length>{};
+  } else if constexpr (S::substr(CurrentPos, 7u) == "{int64}") {
+    return format_param<std::int64_t, CurrentPos, length>{};
+  } else {
+    return true;
+  }
+
+  // else if (s == "{int8}") {
+  //   return std::numeric_limits<std::int8_t>::digits10 + 1u; // 1u for minus
+  // } else if (s == "{uint16}") {
+  //   return std::numeric_limits<std::uint16_t>::digits10;
+  // } else if (s == "{int16}") {
+  //   return std::numeric_limits<std::int16_t>::digits10 + 1u; // 1u for minus
+  // } else if (s == "{uint32}") {
+  //   return std::numeric_limits<std::uint32_t>::digits10;
+  // } else if (s == "{int32}") {
+  //   return std::numeric_limits<std::int32_t>::digits10 + 1u; // 1u for minus
+  // } else if (s == "{uint64}") {
+  //   return std::numeric_limits<std::uint64_t>::digits10;
+  // } else if (s == "{int64}") {
+  //   return std::numeric_limits<std::int64_t>::digits10 + 1u; // 1u for minus
+  // }
+}
+
 constexpr auto calc_size(std::string_view s)
 {
   auto size = 0u;
@@ -115,43 +176,20 @@ constexpr auto calc_size(std::string_view s)
   }
 }
 
-template <typename... Params>
-constexpr auto collect_param_types(std::string_view s, unsigned current_pos,
-                                   Params... params)
-{
-  const auto begin_pos = current.find('{');
-  if (begin_pos == std::string_view::npos) {
-    return types<Params...>{};
-  }
+// template <typename... Params>
+// constexpr auto collect_param_types(std::string_view s, unsigned current_pos,
+//                                    Params... params)
+// {
+//   const auto current = s.substr(current_pos);
+//   const auto begin_pos = current.find('{');
+//   if (begin_pos == std::string_view::npos) {
+//     return types<Params...>{};
+//   }
 
-  const auto end_pos = current.find('}', begin_pos);
-  const auto subs = current.substr(begin_pos, end_pos + 1u);
-  size += length_of(subs);
-  if (subs == "{uint8}") {
-    size += std::numeric_limits<std::uint8_t>::digits10;
-  } else if (subs == "{int8}") {
-    size += std::numeric_limits<std::int8_t>::digits10 + 1u; // 1u for
-  } else if (subs == "{uint16}") {
-    size += std::numeric_limits<std::uint16_t>::digits10;
-  } else if (subs == "{int16}") {
-    size += std::numeric_limits<std::int16_t>::digits10 + 1u; // 1u for
-    minus
-  } else if (subs == "{uint32}") {
-    size += std::numeric_limits<std::uint32_t>::digits10;
-  } else if (subs == "{int32}") {
-    size += std::numeric_limits<std::int32_t>::digits10 + 1u; // 1u for
-    minus
-  } else if (subs == "{uint64}") {
-    size += std::numeric_limits<std::uint64_t>::digits10;
-  } else if (subs == "{int64}") {
-    size += std::numeric_limits<std::int64_t>::digits10 + 1u; // 1u for
-    minus
-  } else if (subs.substr(0, 4) == "{str") {
-    const auto number_subs = subs.substr(4, end_pos - 4);
-    size += stou(number_subs);
-  }
-  current = current.substr(end_pos + 1u);
-}
+//   const auto end_pos = current.find('}', begin_pos);
+//   const auto subs = current.substr(begin_pos, end_pos + 1u);
+//   size += length_of(subs);
+// }
 
 }
 
--- test.cpp
+++ test.cpp
@@ -1,7 +1,15 @@
 #include "infmt.hpp"
 
+#include <type_traits>
+
 int main()
 {
+
+  static_assert(infmt::details::stou("1") == 1u);
+  static_assert(infmt::details::stou("9") == 9u);
+  static_assert(infmt::details::stou("10") == 10u);
+  static_assert(infmt::details::stou("1234567890") == 1234567890u);
+
   static_assert(infmt::details::calc_size("") == 0);
   static_assert(infmt::details::calc_size(" ") == 1);
 
@@ -17,5 +25,48 @@ int main()
   static_assert(infmt::details::calc_size("{uint64}") == 19);
   static_assert(infmt::details::calc_size("{int64}") == 19);
 
+  static_assert(infmt::details::calc_size("{str1}") == 1);
+  static_assert(infmt::details::calc_size("{str9}") == 9);
+  static_assert(infmt::details::calc_size("{str10}") == 10u);
+  static_assert(infmt::details::calc_size("{str1234567890}") == 1234567890u);
+
+  static_assert(infmt::details::calc_size(
+                  "{uint8}{int8}{uint16}{int16}{uint32}{int32}{uint64}{int64}{"
+                  "str123}999 9") ==
+                2 + 3 + 4 + 5 + 9 + 10 + 19 + 19 + 123 + 5);
+
+  // using type_t =
+  //   decltype(infmt::details::format_param_from<0u>(INFMT_STRING("{int8}")));
+
+  {
+    constexpr auto s = INFMT_STRING("{uint8}");
+    static_assert(
+      std::is_same_v<infmt::details::format_param<std::uint8_t, 0, 2>,
+                     decltype(infmt::details::format_param_from<0u>(s))>);
+  }
+  {
+    constexpr auto s = INFMT_STRING("{int8}");
+    static_assert(
+      std::is_same_v<infmt::details::format_param<std::int8_t, 0, 3>,
+                     decltype(infmt::details::format_param_from<0u>(s))>);
+  }
+  {
+    constexpr auto s = INFMT_STRING("{uint16}");
+    static_assert(
+      std::is_same_v<infmt::details::format_param<std::uint16_t, 0, 4>,
+                     decltype(infmt::details::format_param_from<0u>(s))>);
+  }
+  {
+    constexpr auto s = INFMT_STRING("{int16}");
+    static_assert(
+      std::is_same_v<infmt::details::format_param<std::int16_t, 0, 5>,
+                     decltype(infmt::details::format_param_from<0u>(s))>);
+  }
+  {
+    constexpr auto s = INFMT_STRING("{uint32}");
+    static_assert(
+      std::is_same_v<infmt::details::format_param<std::uint32_t, 0, 9>,
+                     decltype(infmt::details::format_param_from<0u>(s))>);
+  }
   constexpr auto formatter = infmt::make_formatter("{int32}");
 }
